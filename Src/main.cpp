67665da61c642e43651a978423b4014ecc5eff72
milaprkpv <91019155+milaprkpv@users.noreply.github.com>
2026-05-14 17:56:56 +0300

    main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,1 +1,25 @@
+#include "../Inc/main.h"
 
+int main() {
+    try {
+        std::string input, output;
+        std::cout << "Specify the path to the input file:";
+        std::cin >> input;
+        std::cout << "Specify the path to the output file:";
+        std::cin >> output;
+
+        Data cur;
+        cur.dread = D_READ;
+        cur.dwrite = D_WRITE;
+        cur.dmove = D_MOVE;
+        cur.ram = RAM;
+
+        Tape Tape_in(input, cur), Tape_out(output, cur);
+        sort(Tape_in, Tape_out, cur.ram);
+    } 
+    catch (std::exception &e) {
+        std::cerr << e.what();
+    }
+
+    return 0;
+}
