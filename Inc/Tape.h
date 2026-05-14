308d8c723b49d348816041109d38f6644d7d2327
milaprkpv <91019155+milaprkpv@users.noreply.github.com>
2026-05-14 23:16:04 +0300

    Tape.h

--- Inc/Tape.h
+++ Inc/Tape.h
@@ -1,1 +1,42 @@
+#include <fstream>
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <iostream>
+#include <filesystem>
 
+struct Data {
+    int readDelay = 0;
+    int writeDelay = 0;
+    int moveDelay = 0;
+};
+
+class Tape {
+public:
+    explicit Tape(const std::string &path);
+    Tape(const std::string &path, const Config &delays);
+    ~Tape();
+
+    Tape(Tape &&other) = default;
+    Tape& operator=(Tape &&other) = default;
+
+    Tape(const Tape&) = delete;
+    Tape& operator=(const Tape&) = delete;
+
+    int& read();
+    void write(const int &elem);
+    bool eot() const;
+    void moveRight();
+    void moveLeft();
+
+private:
+    void readFromFile();
+    void writeToFile();
+
+private:
+    std::string path;
+    std::fstream file;
+    int pos = 0;
+    Data delays;
+    std::vector<int> tape;
+};
